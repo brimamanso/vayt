1d7b9db66878833ce1c1c4067a98bc628c98bc6e
Your Name <you@example.com>
2018-03-22 11:26:39 +0200

    App

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,8 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
-module Papa
+
+module Vayt
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
