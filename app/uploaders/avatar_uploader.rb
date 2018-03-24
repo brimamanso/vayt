952d66232a152adf4bc6b9eaafa7dcfa766e6e3b
Your Name <you@example.com>
2018-03-24 02:23:03 +0200

    we

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,3 +1,12 @@
 class AvatarUploader < CarrierWave::Uploader::Base
-    storage :fog    
+    # Include RMagick or MiniMagick support:
+    # include CarrierWave::RMagick
+    # include CarrierWave::MiniMagick
+  
+    #storage :fog if Rails.env.production?
+  
+    # Choose what kind of storage to use for this uploader:
+    storage :fog
+    # storage :fog
+
 end
