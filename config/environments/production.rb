62f2c567bceeccdd6bc7dd88c600186da747f2a0
Your Name <you@example.com>
2018-03-28 17:06:14 +0200

    we

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,17 +75,19 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: "vayt.herokuapp.com" }
-  config.action_mailer.delivery_method = :smtp 
-  ActionMailer::Base.smtp_settings = {
-    :address          => 'smtp.sendgrid.net',
-    :port             => '587',
-    :authentication   => :plain,
-    :user_name        => ENV["SENDGRID_USERNAME"],
-    :password         => ENV["SENDGRID_PASSWORD"],
-    :domain         => 'heroku.com',
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { :host => 'https://vayt.herokuapp.com' }
+
+  config.action_mailer.smtp_settings = {
+      :user_name => ENV["SENDGRID_USERNAME"],
+    :password => ENV["SENDGRID_PASSWORD"],
+    :address => 'smtp.sendgrid.net',
+    :domain => 'vayt.herokuapp.com',
+    :port => 587,
+    :authentication => :plain,
     :enable_starttls_auto => true
   }
 end
