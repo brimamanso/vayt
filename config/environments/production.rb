f53d7e20f8b9a0f5e536fa9469e8bbf6a59c46f6
Your Name <you@example.com>
2018-03-28 18:09:52 +0200

    we

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,13 +81,21 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
-  config.action_mailer.smtp_settings = {
-      :user_name => ENV["SENDGRID_USERNAME"],
-    :password => ENV["SENDGRID_PASSWORD"],
-    :address => 'smtp.sendgrid.net',
-    :domain => 'vayt.herokuapp.com',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+  
+# using SendGrid's Ruby Library
+# https://github.com/sendgrid/sendgrid-ruby
+require 'sendgrid-ruby'
+include SendGrid
+
+from = Email.new(email: 'test@example.com')
+to = Email.new(email: 'test@example.com')
+subject = 'Sending with SendGrid is Fun'
+content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+mail = Mail.new(from, subject, to, content)
+
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+response = sg.client.mail._('send').post(request_body: mail.to_json)
+puts response.status_code
+puts response.body
+puts response.headers
 end
