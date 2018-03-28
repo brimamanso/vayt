4acd1aa57fb8848792e529e5359f70cf195afbc6
Your Name <you@example.com>
2018-03-28 13:53:25 +0200

    we

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,16 +73,17 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = { host: 'https://vayt.herokuapp.com' }
 
   # Do not dump schema after migrations.
+  config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => 'https://vayt.herokuapp.com' }
   config.action_mailer.smtp_settings = {
-  :address        => "smtp.sendgrid.net",
-  :port           => "587",
-  :authentication => :plain,
-  :user_name      => "<%= ENV['SENDGRID_USERNAME'] %>",
-  :password       => "<%= ENV['SENDGRID_PASSWORD'] %>",
-  :domain         => "heroku.com",
-  :enable_starttls_auto => true
-}
+    :address          => "smtp.sendgrid.net",
+    :port             => "587",
+    :authentication   => :plain,
+    :user_name        => ENV['SENDGRID_USERNAME'],
+    :password         => ENV['SENDGRID_PASSWORD'],
+    :domain         => "heroku.com",
+    :enable_starttls_auto => true
+  }
 end
