42826bd552615963efa9c2eb94c11b058b807a5a
Your Name <you@example.com>
2018-04-09 18:16:40 +0200

    we

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = { host: 'https://wwww.vayte.com' }
+  config.action_mailer.default_url_options = { host: 'https://www.vayte.com' }
   # Do not dump schema after migrations.
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
