f3ef229998b48e355ee234ffae0e6f682ae623f9
Your Name <you@example.com>
2018-04-10 18:14:27 +0200

    we

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,12 +76,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://www.vayte.com' }
   # Do not dump schema after migrations.
   ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => ENV['MAILGUN_DOMAIN'],
-    :authentication => :plain,
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'www.vayte.com',
+  :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
   
