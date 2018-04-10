4c80f4a1e37e1bfbf56d0693902f0fcf28c98592
Your Name <you@example.com>
2018-04-10 17:49:59 +0200

    we

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,9 +80,10 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => ENV['MAILGUN_DOMAIN'],
+    :domain         => 'https://www.vayte.com',
     :authentication => :plain,
   }
+ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.delivery_method = :smtp
   
 end
